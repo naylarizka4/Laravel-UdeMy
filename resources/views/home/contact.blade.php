8d6ce707ace177d8717bf9301e9f771c29f1dc99
naylarizka4 <naylarizkaputria@gmail.com>
2022-08-24 07:45:19 +0700

    1

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page')
+
+@section('content')
+<h1>Contact Us</h1>
+<p>Jl. Pahlawan, Balearjosari</p>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('root');
+
+Route::get('/index', function () {
+    return view('home, index', []);
+})->name('home, index');
+
+Route::get('/contact', function () {
+    return view('home, contact', []);
+})->name('home, contact');
+
+Route::get('/post/{id?}', function ($id=1) {
+    return "Posting Blog ".$id;
+})->name('posts');
+
+Route::get('/days/{id}', function ($id){
+    return "Days".$id;
+})->name('days');
